e00a0e96c3e11b4da9dff58c6d5dc9a51c66847f
Kuan <chenkuansk@ad.garmin.com>
2025-08-08 14:06:50 +0800

    add configurations

---
+++ src/main/java/com/example/freelance_authserver/FreelanceAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelance_authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceAuthserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelance_authserver/FreelanceAuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelance_authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceAuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
