8f914e3aa67e02b9f63a90c53b71ff26817893d7
Kuan <chenkuansk@ad.garmin.com>
2025-08-19 11:07:55 +0800

    add models for authentication

--- src/main/java/com/example/freelance_authserver/entities/FreelanceAuthDetailsSource.java
+++ src/main/java/com/example/freelance_authserver/entities/FreelanceAuthDetailsSource.java
@@ -1,4 +1,15 @@
 package com.example.freelance_authserver.entities;
 
-public class FreelanceAuthDetailsSource {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class FreelanceAuthDetailsSource implements AuthenticationDetailsSource<HttpServletRequest, FreelanceWebAuthenticationDetails> {
+	@Override
+	public FreelanceWebAuthenticationDetails buildDetails(HttpServletRequest request) {
+		return new FreelanceWebAuthenticationDetails(request);
+	}
 }
--- src/main/java/com/example/freelance_authserver/entities/UserEntity.java
+++ src/main/java/com/example/freelance_authserver/entities/UserEntity.java
@@ -1,4 +1,36 @@
 package com.example.freelance_authserver.entities;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+import com.example.freelance_authserver.enums.Gender;
+import com.example.freelance_authserver.enums.UserRole;
+import com.example.freelance_authserver.enums.UserStatus;
+
+@Table(name = "users")
+@Data
+@Builder
 public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+	private String userGUID;
+	private String email;
+	private String password;
+	private UserRole role;
+	private UserStatus status;
+	private String name;
+	private Gender gender;
+	private String description;
+	private LocalDate birthday;
+	private String profilePicture;
+	private LocalDateTime createdDate;
+	private LocalDateTime updatedDate;
 }
