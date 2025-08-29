a5b298534f3ff0624f62f2739a2fa214473e73e5
Kuan <chenkuansk@ad.garmin.com>
2025-08-29 13:29:42 +0800

    app mounted and router displays login page

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+<!--  <div>-->
+<!--    <p>what is going on</p>-->
+<!--    <a href="https://vite.dev" target="_blank">-->
+<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://vuejs.org/" target="_blank">-->
+<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+<!--    </a>-->
+<!--  </div>-->
+<!--  <HelloWorld msg="Vite + Vue" />-->
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
