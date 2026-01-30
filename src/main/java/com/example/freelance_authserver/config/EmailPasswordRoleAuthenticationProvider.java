330cf57269d50b5ef2393677286047a26eeee4a7
Kuan <chenkuansk@ad.garmin.com>
2026-01-30 14:16:29 +0800

    authenticate locally with DB entity

--- src/main/java/com/example/freelance_authserver/config/EmailPasswordRoleAuthenticationProvider.java
+++ src/main/java/com/example/freelance_authserver/config/EmailPasswordRoleAuthenticationProvider.java
@@ -2,6 +2,7 @@ package com.example.freelance_authserver.config;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.example.freelance_authserver.entities.UserCredentialsEntity;
+import com.example.freelance_authserver.repository.UserCredentialsRepository;
 import lombok.RequiredArgsConstructor;
 
 import com.example.freelance_authserver.client.UserManagementServerClient;
@@ -22,7 +26,8 @@ import com.example.freelance_authserver.enums.UserRole;
 @Component
 @RequiredArgsConstructor
 public class EmailPasswordRoleAuthenticationProvider implements AuthenticationProvider {
-	private final UserManagementServerClient userManagementServerClient;
+	private final UserCredentialsRepository userCredentialsRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -30,14 +35,25 @@ public class EmailPasswordRoleAuthenticationProvider implements AuthenticationPr
 		String password = authentication.getCredentials().toString();
 		FreelanceWebAuthenticationDetails details = (FreelanceWebAuthenticationDetails) authentication.getDetails();
 		UserRole role = details.getRole();
-		ResponseEntity<String> authenticationResponse = userManagementServerClient.authenticate(email, role, password);
-		String userGUID = authenticationResponse.getBody();
+		String userGUID = authenticate(email, role, password);
 		if (StringUtils.isNotBlank(userGUID)) {
 			UsernamePasswordAuthenticationToken principal = new UsernamePasswordAuthenticationToken(email, password, List.of(new SimpleGrantedAuthority(role.name())));
 			principal.setDetails(Map.of("userGUID", userGUID));
 			return principal;
 		} else {
 			throw new BadCredentialsException("Invalid password!");
+		}
+	}
+
+	public String authenticate(String email, UserRole role, String password) {
+		Optional<UserCredentialsEntity> optionalUserEntity = userCredentialsRepository.getUserByEmailAndRole(email, role);
+		UserCredentialsEntity userCredentialsEntity = optionalUserEntity
+				.orElseThrow(() -> new BadCredentialsException("User Not Found for email: " + email));
+		boolean isMatched = passwordEncoder.matches(password, userCredentialsEntity.getPassword());
+		if (isMatched) {
+			return userCredentialsEntity.getUserGUID();
+		} else {
+			return null;
 		}
 	}
 
