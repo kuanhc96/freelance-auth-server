2f41426d4c359d1a74d8c99bfb1b0d6935b2c963
Kuan <chenkuansk@ad.garmin.com>
2026-02-25 23:50:36 +0800

    add dump.sql

---
+++ src/main/resources/dump.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user_credentials` (`user_id`, `user_guid`, `email`, `role`, `password`) VALUES (18, '36946828-6696-4b83-be33-6093f2efef70', 'kuandalice@gmail.com', 'INSTRUCTOR', '{noop}Test1234');
+INSERT INTO `user_credentials` (`user_id`, `user_guid`, `email`, `role`, `password`) VALUES (25, 'edf7e0b4-55bf-4302-8685-c546a78a05e9', 'kuantest@example.com', 'STUDENT', '{bcrypt}$2a$10$SR0z9KozjW.9hP/VxSZv9eP.u50.buh0ROsUtYbsdYXRDYTumj0fG');
